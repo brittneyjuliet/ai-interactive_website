7c5ed340af82a9a74a6deeb83a584720da7f79a2
brittneyjuliet <brittneyjuliet@gmail.com>
2025-10-18 02:02:40 +0100

    modified api url

--- sketch.js
+++ sketch.js
@@ -725,7 +725,7 @@ async function sendAudioToAPI(blob){
 		console.log('predicting...');
 		detecting = true;
 
-		const response = await fetch('https://ai-emotion-api.onrender.com',
+		const response = await fetch('https://ai-emotion-api.onrender.com/predict',
 			{
 				method: 'POST',
 				body: formData
