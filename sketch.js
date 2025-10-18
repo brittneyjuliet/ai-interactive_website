fba50ec0ca4bb7f9123b20d9246353009291d190
brittneyjuliet <brittneyjuliet@gmail.com>
2025-10-18 15:44:43 +0100

    sliplane

--- sketch.js
+++ sketch.js
@@ -727,7 +727,7 @@ async function sendAudioToAPI(blob){
 		console.log('predicting...');
 		detecting = true;
 
-		const response = await fetch('https://ai-emotion-api.onrender.com/predict',
+		const response = await fetch('ai-emotion-api.sliplane.app/predict',
 			{
 				method: 'POST',
 				body: formData
